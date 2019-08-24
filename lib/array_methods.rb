d6aec202b435d2f1812a68ba7da3e11976475255
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-24 18:00:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,5 +16,19 @@ end
 
 def find_element_index(array, value_to_find)
   
+  array.length.times { |index| 
+    if array[index] == value_to_find
+      return index
+  }
+end
+
+
+def find_max(array)
   
+  max = 0
+  
+  array.size.times { |index|
+   if array[index] > max
+     
+  }
 end
